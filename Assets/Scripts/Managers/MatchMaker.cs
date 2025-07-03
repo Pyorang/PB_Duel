9ed5a2f3c179fc7d3599d884d34571acedcd4026
YOUKK <kimyoukeng@naver.com>
2025-07-03 22:05:45 +0900

    Feat : Join Session 접속 기능 구현 MatchMaker.cs에 구현함

---
+++ Assets/Scripts/Managers/MatchMaker.cs
@@ -0,0 +1,76 @@
+using Fusion;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/*
+ * 매치메이킹(로비 접속, 방 만들기/입장/종료)을 처리하는 클래스
+ */
+
+//TODO: 퓨전2 깔고 - 1. Runner 프리팹 만들기 2. 코드 테스트하기
+
+public class MatchMaker : MonoBehaviour, INetworkRunnerCallbacks
+{
+    public NetworkRunner Runner;
+    public NetworkRunner runnerPrefab;
+
+    private bool isPrivate = false; // 비공개 여부
+    private string roomcode = null; // 방 이름
+
+    #region Mode Selection에서 Join 버튼 클릭 시 호출되는 함수
+    // Join 버튼 클릭 시 JoinSession에 접속하는 함수
+    public void TryJoinLobby()
+    {
+        StartCoroutine(JoinLobby());
+    }
+
+    IEnumerator JoinLobby()
+    {
+        //TODO: 방 리스트에 로딩 UI 띄우는 코드 추가
+        Runner = Instantiate(runnerPrefab);
+        Runner.AddCallbacks(this); // Runner가 네트워크 이벤트를 감지 시 처리할 수 있도록 등록
+        Task<StartGameResult> task = Runner.JoinSessionLobby(SessionLobby.ClientServer); // 비동기 로비 접속
+        while (!task.IsCompleted)
+        {
+            yield return null;
+        }
+        StartGameResult result = task.Result;
+
+        if (result.Ok)
+        {
+            Debug.Log("로비 접속");
+            //TODO: 로비 UI 세팅 코드 추가
+        }
+        else
+        {
+            Debug.LogError("로비 접속 실패: " + result.Reason);
+            //TODO: 로비 접속 실패 UI 세팅 코드 추가
+        }
+    }
+    #endregion
+
+    #region INetworkRunnerCallbacks
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnConnectFailed(NetworkRunner runner, Fusion.Sockets.NetAddress remoteAddress, Fusion.Sockets.NetConnectFailedReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, System.ArraySegment<byte> data) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+
+    #endregion
+}
--- Assets/Scripts/UI/UIScreen.cs
+++ Assets/Scripts/UI/UIScreen.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 
 /*
- * 로비씬에서 사용되는 UI 스크린
+ * 로비씬에서 사용되는 UI 스크린 관련 클래스
  */
 
 public class UIScreen : MonoBehaviour
 {
     // 다음 UIScreen 띄우기
+    // 다음 로비 UI로 넘어가는 버튼에 연결되는 함수
     public void ShowScreen(UIScreen screen)
     {
         gameObject.SetActive(false);
