fa8769c90337ee09ead603a4c2db5b278117e4f4
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-22 10:28:42 +0900

    Feat: : UserDataManager 작성
    - IUserData 기반 사용자 데이터 관리 매니저 작성
    - UserSettingsData 등록 및 초기화 지원
    - 사용자 데이터 초기화(SetDefaultData), 로드(LoadUserData), 저장(SaveUserData) 기능 추가

--- Assets/Scripts/Managers/UserDataManager.cs
+++ Assets/Scripts/Managers/UserDataManager.cs
@@ -1,14 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UserDataManager : SingletonBehaviour<UserDataManager>
 {
+    public bool ExistsSavedData { get; private set; }
+
+    public List<IUserData> UserDataList { get; } = new();
+
     protected override void Init()
     {
         base.Init();
 
-        
+        ExistsSavedData = PlayerPrefs.GetInt(nameof(ExistsSavedData)) == 1;
+
+        UserDataList.Add(new UserSettingsData());
+    }
+
+    public void SetDefaultData()
+    {
+        foreach (IUserData data in UserDataList)
+        {
+            data.SetDefaultData();
+        }
+    }
+
+    public void LoadUserData()
+    {
+        if (!ExistsSavedData)
+        {
+            return;
+        }
+
+        foreach (IUserData data in UserDataList)
+        {
+            data.LoadData();
+        }
+    }
+
+    public void SaveUserData()
+    {
+        foreach (IUserData data in UserDataList)
+        {
+            if (!data.SaveData())
+            {
+                return;
+            }
+        }
+
+        PlayerPrefs.SetInt(nameof(ExistsSavedData), 1);
+        PlayerPrefs.Save();
+
+        ExistsSavedData = true;
+    }
+
+    public T GetUserData<T>() where T : class, IUserData
+    {
+        return UserDataList.OfType<T>().FirstOrDefault();
     }
 }
