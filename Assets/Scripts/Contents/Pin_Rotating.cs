332bf5a96a3000fffec10557b026f022ac62e93e
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-25 18:13:58 +0900

    Feat : 회전축 수정, 코루틴으로 변환
    - 회전축을 z축으로 통일하기 위해 Rotating Floor의 부모 오브젝트 Rotating Zone 추가
    - 회전축 설정 기능 삭제
    - 회전 방식을 Update() 대신 StartCoroutine() 호출로 변경
    - 회전 방향 조절기 오브젝트의 활성 여부를 저장할 bool 변수 추가

--- Assets/Scripts/Contents/Pin_Rotating.cs
+++ Assets/Scripts/Contents/Pin_Rotating.cs
@@ -1,54 +1,52 @@
 ﻿using UnityEngine;
 
-public enum RotationAxis
-{
-    X,
-    Y,
-    Z
-}
-
 public class Pin_Rotating : MonoBehaviour
 {
     // 회전할 대상 오브젝트
-    [SerializeField] private Transform _target;
+    [SerializeField]
+    private Transform _target;
 
     // 회전 속도 (도/초)
-    [SerializeField] private float _rotationSpeed = 90f;
+    [SerializeField]
+    private float _rotationSpeed = 90f;
 
     // 회전 방향 (시계 방향이면 true)
-    [SerializeField] private bool _isClockwise = true;
+    [SerializeField]
+    private bool _isClockwise = true;
 
-    // 회전할 축
-    [SerializeField] private RotationAxis _rotationAxis = RotationAxis.Z;
+    // 회전 방향 조절기 활성화 상태 여부
+    [SerializeField]
+    private bool _isDirectionRegulatorActive = false;
 
-    private void Update()
+    private void Start()
     {
-        if (_target == null)
-        {
-            return;
-        }
-
-        float direction = _isClockwise ? -1f : 1f;
-        float angle = direction * _rotationSpeed * Time.deltaTime;
-
-        RotateTarget(angle);
+        StartCoroutine(RotateCoroutine());
     }
 
-    private void RotateTarget(float angle)
+    private System.Collections.IEnumerator RotateCoroutine()
     {
-        switch (_rotationAxis)
+        while (true)
         {
-            case RotationAxis.X:
-                _target.Rotate(angle, 0f, 0f);
-                break;
+            if (_target != null)
+            {
+                bool isClockwiseNow;
 
-            case RotationAxis.Y:
-                _target.Rotate(0f, angle, 0f);
-                break;
+                if (_isDirectionRegulatorActive)
+                {
+                    isClockwiseNow = !_isClockwise;
+                }
+                else
+                {
+                    isClockwiseNow = _isClockwise;
+                }
 
-            case RotationAxis.Z:
+                float direction = isClockwiseNow ? -1f : 1f;
+                float angle = direction * _rotationSpeed * Time.deltaTime;
+
                 _target.Rotate(0f, 0f, angle);
-                break;
+            }
+
+            yield return null;
         }
     }
 }
