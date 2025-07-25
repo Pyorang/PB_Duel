c4bc5cd9b83127c68cdc2670e4c4ddce4a330da4
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-25 15:38:57 +0900

    Feat : 회전축 설정 추가
    - 인스펙터 창에서 회전축을 변경할 수 있게 수정
    - 회전하는 바닥(Rotating Floor)에도 스크립트를 적용하기 위해 Pin_RotatingBar -> Pin_Rotating으로 스크립트 파일명 수정
    - Rotating Floor 더미 프리팹에 해당 스크립트 적용

---
+++ Assets/Scripts/Contents/Pin_Rotating.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public enum RotationAxis
+{
+    X,
+    Y,
+    Z
+}
+
+public class Pin_Rotating : MonoBehaviour
+{
+    // 회전할 대상 오브젝트
+    [SerializeField] private Transform _target;
+
+    // 회전 속도 (도/초)
+    [SerializeField] private float _rotationSpeed = 90f;
+
+    // 회전 방향 (시계 방향이면 true)
+    [SerializeField] private bool _isClockwise = true;
+
+    // 회전할 축
+    [SerializeField] private RotationAxis _rotationAxis = RotationAxis.Z;
+
+    private void Update()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        float direction = _isClockwise ? -1f : 1f;
+        float angle = direction * _rotationSpeed * Time.deltaTime;
+
+        RotateTarget(angle);
+    }
+
+    private void RotateTarget(float angle)
+    {
+        switch (_rotationAxis)
+        {
+            case RotationAxis.X:
+                _target.Rotate(angle, 0f, 0f);
+                break;
+
+            case RotationAxis.Y:
+                _target.Rotate(0f, angle, 0f);
+                break;
+
+            case RotationAxis.Z:
+                _target.Rotate(0f, 0f, angle);
+                break;
+        }
+    }
+}
