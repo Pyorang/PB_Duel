6d8bc5fdbc17ee256773fec3d5e15b7ca7f54d4f
YOUKK <kimyoukeng@naver.com>
2025-07-31 21:28:42 +0900

    Feat : JoinSession에서 방 목록 띄우는 기능 구현 - 방 목록인 SessionItemUI 제작(프리팹, 스크립트 둘 다) - SessionListUpdate.cs에 세션 목록 업데이트 기능 구현 - 방 목록 업데이트 기능을 MatchMaker.cs, SessionSetup.cs에 반영

--- Assets/Scripts/Network/SessionSetup.cs
+++ Assets/Scripts/Network/SessionSetup.cs
@@ -1,46 +1,79 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * 방 설정 세팅을 위한 클래스
+ * HostSessionUI 프리팹에 붙는 스크립트
  */
 
 public class SessionSetup : MonoBehaviour
 {
+    // UI 요소 인스펙터에 연결
+    [SerializeField]
+    private TMP_InputField _sessionNameInputField;
+    [SerializeField]
+    private Toggle _isPrivateToggle;
+    [SerializeField]
+    private TMP_InputField _passwordInputField;
+
     // 방 생성 관련 변수들
-    private string sessionName;
-    public string SessionName { get => sessionName; set => sessionName = value; }
-    private bool isPrivate;
+    private string _sessionName;
+    private bool _isPrivate;
+    private string _password;
+    private string _hostName;
+
+    public string SessionName { get => _sessionName; set => _sessionName = value; }
     public bool IsPrivate
     {
-        get => isPrivate; set
+        get => _isPrivate; 
+        set
         {
-            isPrivate = value;
-            //TODO: ApplyConfig() 완성하고 호출하여 방 설정 적용
+            _isPrivate = value;
+            //TODO: ApplyConfig() 호출하여 방 설정 적용
         }
     }
+    public string Password { get => _password; set => _password = value; }
 
-    private Action onTryCreateSession; //TODO: 플레이어 접속 기능 넣기
-    private Action onSessionCreated; //TODO: 세션 생성 UI 기능 넣기
 
+    private Action _onTryCreateSession; //TODO: 플레이어 접속 기능 넣기
+    private Action _onSessionCreated; //TODO: 세션 생성 UI 기능 넣기
+
+
+
+    private void Awake()
+    {
+        _sessionNameInputField.onValueChanged.AddListener((value) => SessionName = value);
+        _isPrivateToggle.onValueChanged.AddListener((value) => IsPrivate = value);
+        _passwordInputField.onValueChanged.AddListener((value) => Password = value);
+    }
+
+    private void Start()
+    {
+        _hostName = GameObject.FindFirstObjectByType<ModeSelectionUI>().GetHostName();
+    }
 
     // 새로운 방 생성
-    // Host Session에서 Confirm 버튼과 연결
+    // Host Session에서 Create 버튼과 연결
     public void TryCreateSession()
     {
         if (MatchMaker.Instance.Runner == null) // 새로운 방 생성
         {
-            onTryCreateSession?.Invoke();
+            _onTryCreateSession?.Invoke();
             MatchMaker.Instance.SetRoomCode(SessionName);
             MatchMaker.Instance.SetPrivate(IsPrivate);
-            MatchMaker.Instance.TryHostSession(() => onSessionCreated?.Invoke());
+            MatchMaker.Instance.SetHostName(_hostName);
+            MatchMaker.Instance.TryHostSession(() => _onSessionCreated?.Invoke());
         }
-        else
+        else // 이미 방이 생성되었다면 설정 변경
         {
+            ApplyConfig();
             Debug.LogError("이미 방이 생성되어 있습니다. 방을 나가고 다시 시도해주세요.");
         }
     }
 
+    //TODO: ApplyConfig 완성하기
     // 기존 방 설정 변경
     public void ApplyConfig()
     {
--- Assets/Scripts/UI/Lobby/HostSessionUI.cs
+++ Assets/Scripts/UI/Lobby/HostSessionUI.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+ * HostSessionUI 프리팹에 붙는 스크립트
+ */
+
 public class HostSessionUI : BaseUI
 {
     [SerializeField]
-    private Button backButton;
+    private Button _backButton, _createButton;
 
     private void Start()
     {
-        backButton.onClick.AddListener(OnClickCloseButton);
+        _backButton.onClick.AddListener(OnClickCloseButton);
+        _createButton.onClick.AddListener(gameObject.GetComponent<SessionSetup>().TryCreateSession);
+        //createButton.onClick.AddListener(OnClickCreateButton);
+    }
+
+    // InSession UI 머지되면 작성하기
+    public void OnClickCreateButton()
+    {
+        //UIManager.Instance.OpenUI<>();
     }
 }
