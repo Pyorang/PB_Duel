f8ddefba6083edb4dd2b86d54d51ad9c8b133009
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-31 19:35:33 +0900

    Refactor : _isDirectionRegulatorActive 변수 삭제
    - 회전 방향 조절기 활성화 여부(_isDirectionRegulatorActive) 변수 삭제

--- Assets/Scripts/Contents/Pin_Rotating.cs
+++ Assets/Scripts/Contents/Pin_Rotating.cs
@@ -11,9 +11,6 @@ public class Pin_Rotating : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed = 90f;
 
-    [SerializeField]
-    private bool _isDirectionRegulatorActive = false;
-
     private void Start()
     {
         StartCoroutine(RotateCoroutine());
@@ -25,18 +22,7 @@ public class Pin_Rotating : MonoBehaviour
         {
             if (_target != null)
             {
-                bool isClockwiseNow;
-
-                if (_isDirectionRegulatorActive)
-                {
-                    isClockwiseNow = !IsClockwise;
-                }
-                else
-                {
-                    isClockwiseNow = IsClockwise;
-                }
-
-                float direction = isClockwiseNow ? -1f : 1f;
+                float direction = IsClockwise ? -1f : 1f;
                 float angle = direction * _rotationSpeed * Time.deltaTime;
 
                 _target.Rotate(0f, 0f, angle);
