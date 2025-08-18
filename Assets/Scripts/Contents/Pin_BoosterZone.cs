340d94e36262aa68242abe2481bf0ef607e1c2c6
ubninin <144878603+ubninin@users.noreply.github.com>
2025-08-18 16:40:52 +0900

    Feat : Pin_BoosterZone 작성
    - Pin_BoosterZone.cs 작성

---
+++ Assets/Scripts/Contents/Pin_BoosterZone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Pin_BoosterZone : MonoBehaviour
+{
+    public int speedMultiplier = 2;
+    public Pin_Ball pinBall; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball")) 
+        {
+            if (pinBall != null)
+            {
+                Debug.Log("부스터존 진입");
+                pinBall.MultiplyVelocity(speedMultiplier);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ball")) 
+        {
+            if (pinBall != null)
+            {
+                Debug.Log("부스터존 ");
+                pinBall.MultiplyVelocity(1f / speedMultiplier);
+            }
+        }
+    }
+}
